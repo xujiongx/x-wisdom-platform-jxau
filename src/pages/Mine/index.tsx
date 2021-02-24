78a3a937626505addb4310bd86b28f91497a56b5
MYSOFT\xuj16 <xuj16@mingyuanyun.com>
2021-02-24 21:46:38 +0800

    ✨feat: 登录

--- src/pages/Mine/index.tsx
+++ src/pages/Mine/index.tsx
@@ -1,14 +1,55 @@
-import React, { useEffect } from "react";
-import { View } from "@tarojs/components";
+import Taro from "@tarojs/taro";
+import React, { useEffect, useState } from "react";
+import { Button, View } from "@tarojs/components";
 import "./index.less";
 
 export default function Mine() {
+  const [userId, setUserId] = useState("");
   useEffect(() => {
-    console.log("我的");
+    getSessions();
   }, []);
+  const getSessions = () => {
+    Taro.login({
+      success: (res) => {
+        console.log(res);
+        Taro.request({
+          url: "http://localhost:3000/auth/getSession",
+          method: "GET",
+          data: {
+            code: res.code,
+          },
+        })
+          .then((result) => {
+            Taro.setStorageSync("openid", result.data.data.openid);
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+      },
+    });
+  };
+  const getUserInfo = (e: any) => {
+    const userInfo = e.detail.userInfo;
+    userInfo.openid = Taro.getStorageSync("openid");
+    console.log(userInfo);
+    Taro.request({
+      url: "http://localhost:3000/auth/login",
+      method: "GET",
+      data: { userInfo },
+    }).then((res) => {
+      console.log(res.data.data.userId);
+      Taro.setStorageSync("userId", res.data.data.userId);
+      setUserId(Taro.getStorageSync("userId"));
+    });
+  };
   return (
     <View>
       <View>Hello Word</View>
+      {!userId && (
+        <Button openType="getUserInfo" onGetUserInfo={getUserInfo}>
+          登录
+        </Button>
+      )}
     </View>
   );
 }
--- src/pages/Square/index.config.ts
+++ src/pages/Square/index.config.ts
@@ -1,3 +1,5 @@
 export default {
-  navigationBarTitleText: '广场'
-}
+  navigationBarTitleText: "广场",
+  navigationBarTextStyle: "white",
+  navigationBarBackgroundColor: "#6D70FF",
+};
